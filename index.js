013316419172a54d6ac4bbb24bc24dca1ca24795
Stephen Kettley <kettleystephen@gmail.com>
2023-09-15 17:18:36 +0200

    implement start game button functionality

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-// const startGameButton = document.getElementById("start-button");
-// const gridBlocks = document.getElementsByClassName("grid-block");
+const startGameButton = document.getElementById("start-button");
+const gridBlocks = document.getElementsByClassName("grid-block-animated");
+const nextLevelButton = document.getElementById("next-level-button");
+const gameplayText = document.getElementById("gameplay-text");
+const gameDescription = document.getElementById("game-desc");
 
-// window.onload = () => {
-//   startGameButton.style.display = "block";
-// };
+console.log(gridBlocks);
 
-// startGameButton.addEventListener("click", () => {
-//   startGameButton.style.display = "none";
-// });
+let timeout;
+
+window.onload = () => {
+  startGameButton.style.display = "block";
+  nextLevelButton.style.display = "none";
+  gameplayText.style.display = "none";
+  gameDescription.style.display = "block";
+};
+
+startGameButton.addEventListener("click", () => {
+  startGameButton.style.display = "none";
+  Array.from(gridBlocks).forEach(function (block) {
+    block.classList.remove("grid-block-animated");
+    block.classList.add("grid-block-normal");
+    block.classList.add("grid-block-start");
+  });
+  gameDescription.style.display = "none";
+  gameplayText.style.display = "block";
+});
