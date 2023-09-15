f727039ae7e1b6d7209025296f0c44fadd9b4f02
Stephen Kettley <kettleystephen@gmail.com>
2023-09-15 16:02:48 +0200

    implement grid design

---
+++ index.js
@@ -0,0 +1,10 @@
+// const startGameButton = document.getElementById("start-button");
+// const gridBlocks = document.getElementsByClassName("grid-block");
+
+// window.onload = () => {
+//   startGameButton.style.display = "block";
+// };
+
+// startGameButton.addEventListener("click", () => {
+//   startGameButton.style.display = "none";
+// });
