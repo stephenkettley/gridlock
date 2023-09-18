15b6add54f6d831cb3326daa201ef56f8ff2cd54
Stephen Kettley <kettleystephen@gmail.com>
2023-09-18 11:29:50 +0200

    start sequence flashing logic

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
+const grid = document.getElementById("grid");
 const startGameButton = document.getElementById("start-button");
-const gridBlocks = document.getElementsByClassName("grid-block-animated");
+const gridBlocks = document.getElementsByClassName("grid-block");
 const nextLevelButton = document.getElementById("next-level-button");
-const gameplayText = document.getElementById("gameplay-text");
+const flashSequenceButton = document.getElementById("flash-sequence-button");
 const gameDescription = document.getElementById("game-desc");
-
-console.log(gridBlocks);
-
-let timeout;
+let sequenceNumber = 1;
+const gridBlockNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 window.onload = () => {
   startGameButton.style.display = "block";
   nextLevelButton.style.display = "none";
-  gameplayText.style.display = "none";
+  flashSequenceButton.style.display = "none";
   gameDescription.style.display = "block";
+  grid.style.display = "grid";
 };
 
 startGameButton.addEventListener("click", () => {
   startGameButton.style.display = "none";
   Array.from(gridBlocks).forEach(function (block) {
     block.classList.remove("grid-block-animated");
-    block.classList.add("grid-block-normal");
+    block.classList.remove("grid-block-flashing");
     block.classList.add("grid-block-start");
+    gameDescription.style.color = "#b055ff";
+    gameDescription.textContent = `Sequence ${sequenceNumber}`;
+    flashSequenceButton.style.display = "block";
   });
-  gameDescription.style.display = "none";
-  gameplayText.style.display = "block";
 });
+
+nextLevelButton.addEventListener("click", () => {
+  sequenceNumber++;
+  nextLevelButton.style.display = "none";
+  gameDescription.textContent = `Sequence ${sequenceNumber}`;
+  Array.from(gridBlocks).forEach(function (block) {
+    block.classList.remove("grid-block-animated");
+    block.classList.remove("grid-block-flashing");
+    block.classList.add("grid-block-start");
+    gameDescription.style.color = "#b055ff";
+    gameDescription.textContent = `Sequence ${sequenceNumber}`;
+    flashSequenceButton.style.display = "block";
+  });
+});
+
+flashSequenceButton.addEventListener("click", () => {
+  flashSequenceButton.style.display = "none";
+  Array.from(gridBlocks).forEach(function (block) {
+    block.classList.remove("grid-block-start");
+    block.classList.add("grid-block-flashing");
+  });
+  nextLevelButton.style.display = "block";
+  flashSequence(sequenceNumber);
+});
+
+function flashSequence(sequenceNumber) {
+  let flashingSequence = [];
+  let block;
+  for (let i = 1; i <= sequenceNumber; i++) {
+    block = Math.floor(Math.random() * 9) + 1;
+    flashingSequence.push(block);
+  }
+  console.log(flashingSequence);
+}
